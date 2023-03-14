1f92f946b2b6895a7517aa26786a2e74c101608e
Imran Yousaf <imranyousaf.fcs@nxvt.com>
2023-03-14 13:27:13 +0500

    worked on email sending

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -51,8 +51,15 @@ class NewUserController extends Controller
         $user->save();
 
         // Send an activation email to the user
-        Mail::to($user->email)->send(new UserActivationEmail($token));
+        //Mail::to($user->email)->send(new UserActivationEmail($token));
+        $mail = $user->email;
 
+        Mail::send(new UserActivationEmail($token), function ($message) use ($mail) {
+            $message->to($mail);
+            $message->from(env('MAIL_FROM_Email'), env('MAIL_FROM_NAME'));
+            $message->subject('GrowthTracker Nextbridge User Activation');
+        });
+        
         // Return a success response with a message
 
         return response()->json(['message' => 'Account Created Successfully Please Verify The Mail For Login'], 200);
